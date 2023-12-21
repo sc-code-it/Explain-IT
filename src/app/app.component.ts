abe84745d72c4c3fcf91931ad6bea8d1345ef963
Steve C <carp-dev.sc@outlook.com>
2023-12-21 12:27:21 +0000

    initial layout and colours

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,23 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+
 import { HeaderComponent } from './components/ui/header/header.component';
+import { PositiveInfoComponent } from './components/pages/positive-info/positive-info.component';
+import { NegativeInfoComponent } from './components/pages/negative-info/negative-info.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, HeaderComponent],
+  imports: [
+    CommonModule,
+    RouterOutlet,
+    HeaderComponent,
+    PositiveInfoComponent,
+    NegativeInfoComponent,
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'Explain-IT';
--- src/app/components/pages/positive-info/positive-info.component.ts
+++ src/app/components/pages/positive-info/positive-info.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { CardComponent } from '../../ui/card/card.component';
 
 @Component({
   selector: 'app-positive-info',
   standalone: true,
-  imports: [],
+  imports: [CardComponent],
   templateUrl: './positive-info.component.html',
   styleUrl: './positive-info.component.css'
 })
