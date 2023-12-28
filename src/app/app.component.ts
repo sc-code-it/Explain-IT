371bcb0290971f3eb0fa57e43b4d2a4ff4a28011
Steve C <carp-dev.sc@outlook.com>
2023-12-28 14:40:10 +0000

    remove unused packages

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,30 +2,14 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 
-import * as cv from "@techstark/opencv-js";
-
 import { HeaderComponent } from './components/ui/header/header.component';
 import { SideInfoComponent } from './components/ui/side-info/side-info.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [
-    CommonModule,
-    RouterOutlet,
-
-    HeaderComponent,
-    SideInfoComponent
-  ],
+  imports: [CommonModule, RouterOutlet, HeaderComponent, SideInfoComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
-export class AppComponent {
-
-  constructor() {
-    setTimeout(() => {
-      (window as any).cv = cv;
-    }, 1000);
-  }
-
-}
+export class AppComponent {}
