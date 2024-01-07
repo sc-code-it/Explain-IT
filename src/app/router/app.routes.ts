c6bc5718899f5413e2b1f85141c7b1d31daf6661
Steve C <carp-dev.sc@outlook.com>
2024-01-07 15:57:49 +0000

    base point for Todo

--- src/app/router/app.routes.ts
+++ src/app/router/app.routes.ts
@@ -1,8 +1,3 @@
 import { Routes } from '@angular/router';
-import { PositiveInfoComponent } from '../components/pages/positive-info/positive-info.component';
-import { DashboardComponent } from '../components/pages/dashboard/dashboard.component';
 
-export const routes: Routes = [
-  { path: '', component: DashboardComponent, pathMatch: 'full'},
-  { path: 'pos-info', component: PositiveInfoComponent },
-];
+export const routes: Routes = [];
